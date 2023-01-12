f9064ed26b9f554a78f15b23759a4475a97ad027
Davide Primino <dprimino@olivelliputelli.it>
2023-01-12 12:11:41 +0100

    Statistiche

--- ScuoloaDbApp/Shared/Classe.cs
+++ ScuoloaDbApp/Shared/Classe.cs
@@ -9,5 +9,5 @@ public partial class Classe
     public string Ubicazione { get; set; } = null!;
     [StringLength(255)]
     public string? Note { get; set; }
-    public virtual ICollection<Studente> Studenti { get; } = new List<Studente>();
+    public List<Studente> Studenti { get; } = new List<Studente>();
 }
--- ScuoloaDbApp/Shared/Studente.cs
+++ ScuoloaDbApp/Shared/Studente.cs
@@ -6,24 +6,12 @@ public class Studente
 {
     [Key]
     public int Matricola { get; set; }
-
-    [StringLength(100)]
     public string Cognome { get; set; } = null!;
-
-    [StringLength(100)]
     public string Nome { get; set; } = null!;
-
-    [StringLength(255)]
     public string? Indirizzo { get; set; }
-
-    [StringLength(20)]
     public string? Telefono { get; set; }
-
-    [StringLength(255)]
     public string? Email { get; set; }
     public bool Diplomato { get; set; }
-
-    public string SiglaClasse { get; set; } = null!;
-
-    // public virtual Classe SiglaClasseNavigation { get; set; } = null!;
+    public string ClasseSigla { get; set; } = null!;
+    public Classe Classe { get; set; } = null!;
 }
